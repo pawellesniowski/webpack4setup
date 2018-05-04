7c6a374c521bf16f45f05df3afefa77e9f13fcff
Pawel Lesniowski <pawel_lesniowski@epam.com>
2018-05-04 12:11:40 +0200

    adds scss to configuration

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./scss/main.scss";
+
+const a = ()=>{
+    console.log('hello es6')
+};
+
+console.log("hello webpack 222");
+
+console.log("hello webpack 222");
+
+console.log("hello webpack 222");
+
+console.log("hello webpack 222");
+
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: path.resolve('src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {}
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
+};
